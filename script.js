d94d444820c9203417ba9ab413bb1331faebda62
Shivanshh369 <shivanshhh369@gmail.com>
2025-04-27 16:08:52 +0530

    loaderUpdated

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ tl.from(".line h1", {
   duration: 0.6,
   delay: 0.5,
 });
-tl.from("#line1-part1, .line h2", {
+tl.from("#line1-part1", {
   opacity: 0,
   onStart: function () {
     var h5timer = document.querySelector("#line1-part1 h5");
@@ -20,8 +20,25 @@ tl.from("#line1-part1, .line h2", {
   },
 });
 
+tl.to(".line h2", {
+  animationName: "anime",
+  opacity: 1,
+});
+
 tl.to("#loader", {
   opacity: 0,
-  duration: 0.4,
+  duration: 0.2,
   delay: 4,
+});
+
+tl.from("#page1", {
+  delay: 0.2,
+  y: 1600,
+  opacity: 0,
+  duration: 0.5,
+  ease: Power4,
+});
+
+tl.to("#loader", {
+  display: "none",
 });
